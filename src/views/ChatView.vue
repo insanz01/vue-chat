40e6d67568be7349863a2eb11de0c73ee94c48c7
insanz01 <insankamil002@gmail.com>
2023-01-22 17:56:46 +0700

    chat feature

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -2,76 +2,79 @@
   <div>
     <ul class="list-group">
       <li v-for="chat in chats" :key="chat.id" class="list-group-item">
-        {{ chat.source.name }} > chat.message
+        {{ chat.source.name }} > {{ chat.message }}
       </li>
     </ul>
+    <input type="text" v-model="message">
+    <button @click="sendMessage()">kirim</button>
   </div>
 </template>
 
 <script>
-  const name = 'anonym';
+export default {
+  name: "chat-view",
+  data() {
+    return {
+      userId: 1,
+      name: "anonym",
+      message: "",
+      chats: [],
+      randomString:
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+    };
+  },
+  created() {
+    const name = prompt("What is your name?");
 
-  const socket = io('http://localhost:3000')
-
-  socket.emit('new-user', name)
-
-  socket.on('chat-message', data => {
-    appendMessage(`${data.name}: ${data.message}`)
-  })
-
-  socket.on('user-connected', name => {
-    appendMessage(`${name} connected`)
-  })
-
-  socket.on('user-disconnected', name => {
-    appendMessage(`${name} disconnected`)
-  })
-
-  export default {
-    name: "chat-view",
-    data() {
-      return {
-        userId: 1,
-        name: "anonym",
-        message: "",
-        chats: [],
-        randomString: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-      }
+    this.name = name;
+  },
+  sockets: {
+    connect: function () {
+      console.log('socket connected')
     },
-    created() {
-      const name = prompt('What is your name?')
-
-      this.name = name;
+    newUser: function () {
+      console.log('socket and new user connected')
     },
-    methods: {
-      generateString: function(length) {
-          let result = ' ';
-          const charactersLength = characters.length;
-          for ( let i = 0; i < length; i++ ) {
-              result += characters.charAt(Math.floor(Math.random() * charactersLength));
-          }
-
-          return result;
-      },
-      appendMessage: function(message) {
-        const data = {
-          id: this.generateString(30),
-          message,
-          source: {
-            user_id: this.userId,
-            name: this.name
-          }
-        };
-
-        this.chats.push(data);
-      },
-      sendMessage: function() {
-        socket.emit("send-chat-message", this.message);
-        
-        this.appendMessage(this.message);
-
-        this.message = "";
-      }
+    chatMessage: function (data) {
+      console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
+      console.log(data.message);
+      this.appendMessage(data.message);
     }
-  }
+  },
+  methods: {
+    generateString: function (length) {
+      let result = " ";
+      const charactersLength = this.randomString.length;
+      for (let i = 0; i < length; i++) {
+        result += this.randomString.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+
+      return result;
+    },
+    appendMessage: function (data) {
+
+      this.chats.push(data);
+    },
+    sendMessage: function () {
+      console.log(this.message);
+
+      const data = {
+        id: this.generateString(30),
+        message: this.message,
+        source: {
+          user_id: this.userId,
+          name: this.name,
+        },
+      };
+
+      this.$socket.emit("sendChatMessage", data);
+
+      this.appendMessage(data);
+
+      this.message = "";
+    },
+  },
+};
 </script>
